2ab10c767f4da6b5c7d5bfc420eeaaca95d3d27c
Austin Yun <132804202+softwindflower@users.noreply.github.com>
2023-11-26 11:37:40 -0600

    updated accountFunctions

--- accountFunctions.c
+++ accountFunctions.c
@@ -1,33 +1,96 @@
-#include <stdio.h>
-// Functions for viewing current amount of money.
-void viewFunds(char userName[20], char passWord[20])
-{
+void addFunds(char *userName){
     FILE* file;
-    file = fopen(userName, "r");
-
-    int balance;
-    int digits[20];
-    char c;
-
-    for(int i=0; i<strlen(passWord)+1; i++){
-        c = fgetc(file);
+    float balance, userDeposit;
+    
+    printf("Please enter the amount you would like to deposit: \n");
+    scanf("%f", userDeposit);
+    
+    file = fopen(userName, "r+");
+    
+    if (file == NULL) {
+        fprint("Username not found!\n");
+        return;
     }
 
-    printf("You have: ");
-    for(int i = 0; i<16; i++){
-        if(c == EOF) break;
-        c = fgetc(file);
-        printf("%c", c);
+    fseek(file, 0, SEEK_SET);
+
+    int count = 0;
+    char ch;
+
+    //count lines
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            count++;
+        }
     }
-    printf("funds in your account.\n\n");
+    //return pointer to the beginning of the file 
+    fseek(file, 0, SEEK_SET);
+    //find last line
+    int currentLine = 0;
+    while (currentLine < count - 1) {
+        if (fgetc(file) == '\n') {
+            currentLine++;
+        }
+    }
+    //read balance from last line
+    fscanf(file, "%lf", &balance);
+    
+    balance += userDeposit;
+    
+    printf("Your new balance is %f.\n", balance);
+    
+    fseek(file, 0, SEEK_END);
+    //write new balance to a new line in the file
+    fprintf(file, "\n%f", balance);
+    
     fclose(file);
-    return;
-}
-// TODO: Complete these functions
-void addFunds(){
-
 }
 
-void withdrawFunds(){
+void withdrawFunds(char *userName){
+    FILE* file;
+    float balance, userWithdraw;
+    
+    printf("Please enter the amount you would like to withdraw: \n");
+    scanf("%f", userWithdraw);
+    
+    file = fopen(userName, "r+");
+    
+    if (file == NULL) {
+        fprint("Username not found!\n");
+        return;
+    }
 
+    fseek(file, 0, SEEK_SET);
+
+    int count = 0;
+    char ch;
+
+    //count lines
+    while ((ch = fgetc(file)) != EOF) {
+        if (ch == '\n') {
+            count++;
+        }
+    }
+    //return pointer to the beginning of the file 
+    fseek(file, 0, SEEK_SET);
+    //find last line
+    int currentLine = 0;
+    while (currentLine < count - 1) {
+        if (fgetc(file) == '\n') {
+            currentLine++;
+        }
+    }
+    //read balance from last line
+    fscanf(file, "%lf", &balance);
+    
+    balance -= userWithdraw;
+    
+    printf("Your new balance is %f.\n", balance);
+    
+    fseek(file, 0, SEEK_END);
+    //write new balance to a new line in the file
+    fprintf(file, "\n%f", balance);
+    
+    fclose(file);
+    
 }
